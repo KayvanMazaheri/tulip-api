686e68d28cd5290b99555308281291dac2621d0c
Touhid Arastu <touhid.arastu@gmail.com>
2018-09-08 01:31:59 +0430

    change entrypoint of app to bin/www some cleaning

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
+const path = require('path')
+
 const express = require('express')
-const bodyParser = require('body-parser')
+const logger = require('morgan')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const connectMongo = require('connect-mongo')
-const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const createError = require('http-errors')
+const cookieParser = require('cookie-parser')
 
 const authRoutes = require('./routes/auth')
 const indexRouter = require('./routes')
 const storyRouter = require('./routes/story')
 
-// Start: Database initialization and configs
-
-// Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/tehran', { useNewUrlParser: true })
-const db = mongoose.connection
-
-// Handle mongo error
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function () {
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
+mongoose.connection.once('open', function () {
   console.log('we\'re connected!')
 })
 
-// End: Database initialization and configs
-
 const app = express()
 
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
+app.use(logger('dev'))
+app.use(express.json())
+app.use(cookieParser())
 app.use('/static', express.static('public'))
-
-// set morgan to log info about our requests for development use.
-app.use(morgan('dev'))
-
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-const MongoStore = connectMongo(session)
+app.use('/auth', authRoutes)
+app.use('/', indexRouter)
+app.use('/stories', storyRouter)
 
 // use sessions for tracking logins
+const MongoStore = connectMongo(session)
 app.use(session({
   secret: 'work hard',
   resave: true,
@@ -45,12 +45,24 @@ app.use(session({
     expires: 600000
   },
   store: new MongoStore({
-    mongooseConnection: db
+    mongooseConnection: mongoose.connection
   })
 }))
 
-app.use('/auth', authRoutes)
-app.use('/', indexRouter)
-app.use('/stories', storyRouter)
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
-app.listen(3000, () => console.log('server listening on 3000'))
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
